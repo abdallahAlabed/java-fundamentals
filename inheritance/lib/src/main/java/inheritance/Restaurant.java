e94e50945e8c1ee681bdcb0f33ec6781c7d4e57f
abdallahAlabed <81149825+abdallahAlabed@users.noreply.github.com>
2021-08-08 16:31:28 +0300

    Update Restaurant.java

--- inheritance/lib/src/main/java/inheritance/Restaurant.java
+++ inheritance/lib/src/main/java/inheritance/Restaurant.java
@@ -1,21 +1,19 @@
-package inheritance;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Restaurant implements Reviewable {
     private int stars;
     private int priceCategory;
     private String name;
-    private List <Review> reviews = new ArrayList<Review>();
+    private List <Review> reviews;
 
-    public Restaurant(int priceCategory, String name) {
+    public Restaurant( String name,int priceCategory) {
         if(this.priceCategory >= 0){
             this.priceCategory = Math.min(priceCategory, 5);
         }else{
             this.priceCategory = Math.max(priceCategory, 0);
         }
         this.name = name;
+        this.reviews = new ArrayList<Review>();
         }
 
         public String getName () {
@@ -37,15 +35,16 @@ public class Restaurant implements Reviewable {
         public int getPriceCategory () {
             return priceCategory;
         }
+        
+        public List <Review> getReviews() {
+        return reviews;
+        }
 
         public void setPriceCategory ( int priceCategory){
             this.priceCategory = priceCategory;
         }
 
-        public List<Review> getReviews () {
-            return reviews;
-        }
-
+       
         public void setReviews (List < Review > reviews) {
             this.reviews = reviews;
         }
@@ -74,12 +73,12 @@ public class Restaurant implements Reviewable {
             }
             avg = sum /num;
             avg = Math.round(avg * 10.0) / 10.0;
-            this.setStars(avg);
+            this.setStars((int)avg);
         }
 
         @Override
         public String reviewtoString () {
-            LinkedList<Review> reviews = getReviews();
+            List<Review> reviews = getReviews();
             StringBuilder review = new StringBuilder();
             for (Review value : reviews) {
                 review.append(value.toString());
